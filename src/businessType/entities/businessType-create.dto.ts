7c5a167041b0aab2899211fab3e44463cde43ddf
diegob <diegob@booster.software>
2023-09-03 17:18:14 -0300

    reservation possible fixes and businessType implementation

--- src/businessType/entities/businessType-create.dto.ts
+++ src/businessType/entities/businessType-create.dto.ts
@@ -1,11 +1,16 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 
 export class BusinessTypeCreateDto {
+    @IsString()
     id?: string;
 
+
+    @IsString()
     @IsNotEmpty()
     name: string;
 
+    @IsString()
+    @IsOptional()
     description: string;
 }
