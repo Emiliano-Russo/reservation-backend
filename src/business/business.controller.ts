8c48bc5d24e84595043f7a6be320acfbc9eca636
diegob <diegob@booster.software>
2023-09-05 00:13:48 -0300

    first business structure

---
+++ src/business/business.controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from "@nestjs/common";
+import { BusinessService } from "./business.service";
+
+@Controller('business')
+export class BusinessController {
+
+    constructor(
+        private readonly businessService: BusinessService,
+    ) {
+    }
+}
