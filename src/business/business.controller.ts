df73ec2349c4d80629c0130efac61e1fd706ee89
diegob <diegob@booster.software>
2023-09-06 22:55:31 -0300

    getters for business module

--- src/business/business.controller.ts
+++ src/business/business.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { BusinessService } from "./business.service";
 
 @Controller('business')
@@ -7,5 +7,33 @@ export class BusinessController {
     constructor(
         private readonly businessService: BusinessService,
     ) {
+    }
+
+    @Get()
+    async getBusinessById(
+        @Query('businessId') businessId: string
+    ) {
+        return this.businessService.getBusinessById(businessId);
+    }
+
+    @Get("/owner")
+    async getBusinessByOwnerId(
+        @Query('ownerId') ownerId: string
+    ) {
+        return this.businessService.getBusinessByOwnerId(ownerId);
+    }
+
+    @Get("/type")
+    async getBusinessByTypeId(
+        @Query('typeId') typeId: string
+    ) {
+        return this.businessService.getBusinessByTypeId(typeId);
+    }
+
+    @Get("/premiumSubscription")
+    async getBusinessByActivePremiumSubscriptionId(
+        @Query('activePremiumSubscriptionId') activePremiumSubscriptionId: string
+    ) {
+        return this.businessService.getBusinessByActivePremiumSubscriptionId(activePremiumSubscriptionId);
     }
 }
--- src/business/business.service.ts
+++ src/business/business.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from "@nestjs/common";
+import { Business } from "./entities/business.entity";
 
 @Injectable()
 export class BusinessService {
 
     constructor() {
+    }
+
+    async getBusinessById(id: string) {
+        const business = await Business.scan("id").eq(id).exec();
+        return business;
+    }
+
+    async getBusinessByOwnerId(ownerId: string): Promise<any> {
+        const business = await Business.scan("ownerId").eq(ownerId).exec();
+        return business;
+    }
+
+    async getBusinessByTypeId(typeId: string): Promise<any> {
+        const business = await Business.scan("typeId").eq(typeId).exec();
+        return business;
+    }
+
+    async getBusinessByActivePremiumSubscriptionId(activePremiumSubscriptionID: string): Promise<any> {
+        const business = await Business.scan("activePremiumSubscriptionID").eq(activePremiumSubscriptionID).exec();
+        return business;
     }
 }
