a2258af8b482722ed7d48e1de4043f7809a998d1
Emiliano Russo <russo.emiliano97@gmail.com>
2023-09-11 20:21:35 -0300

    feature/public cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
+  //en produccion  origin: ['http://miapp.com', 'http://otrodominio.com']
+  app.enableCors({
+    origin: '*', // Permitir todos los orígenes (no recomendado para producción)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
