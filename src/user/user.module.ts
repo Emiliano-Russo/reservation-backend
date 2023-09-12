7da0bc8661bc72738d6551fe889818828604b13e
Emiliano Russo <russo.emiliano97@gmail.com>
2023-09-11 23:27:08 -0300

    feature/return token when user created

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { S3Service } from 'src/shared/s3.service';
 import { ReservationModule } from 'src/reservation/reservation.module';
+import { AuthModule } from 'src/auth/auth.module';
 @Module({
-    imports: [ReservationModule],
-    controllers: [UserController],
-    providers: [UserService, S3Service],
-    exports: [UserService],
+  imports: [ReservationModule, forwardRef(() => AuthModule)],
+  controllers: [UserController],
+  providers: [UserService, S3Service],
+  exports: [UserService],
 })
-export class UserModule { }
+export class UserModule {}
