c474a49f7e4bf38891de6a7f3bd691545610a8b1
diegob <diegob@booster.software>
2023-09-16 02:20:52 -0300

    improvments

--- src/business/business.controller.ts
+++ src/business/business.controller.ts
@@ -1,159 +1,143 @@
 import {
-    BadRequestException,
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    Patch,
-    Post,
-    Query,
-    UploadedFile,
-    UploadedFiles,
-    UseInterceptors,
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
 } from '@nestjs/common';
 import { BusinessService } from './business.service';
 import { BusinessCreateDto } from './entities/business-create.dto';
 import { BusinessUpdateDto } from './entities/business-update.dto';
 import {
-    FileFieldsInterceptor,
-    FileInterceptor,
-    FilesInterceptor,
+  FileFieldsInterceptor,
+  FileInterceptor,
+  FilesInterceptor,
 } from '@nestjs/platform-express';
 import { PaginationParametersDto } from "src/helpers/pagination-parameters.dto";
 
 @Controller('business')
 export class BusinessController {
-    constructor(private readonly businessService: BusinessService) { }
+  constructor(private readonly businessService: BusinessService) { }
 
+  @Get("/owner")
+  async getBusinessPaginatedByOwnerId(
+    @Query('ownerId') ownerId: string,
+    @Body() pagination: PaginationParametersDto
+  ) {
+    return this.businessService.getBusinessPaginatedByOwnerId(ownerId, pagination);
+  }
 
-    @Get()
-    async getBusinessPaginatedById(
-        @Query('businessId') businessId: string,
-        @Body() pagination: PaginationParametersDto
-    ) {
-        return this.businessService.getBusinessPaginatedById(businessId, pagination);
-    }
+  @Get("/type")
+  async getBusinessPaginatedByTypeId(
+    @Query('typeId') typeId: string,
+    @Body() pagination: PaginationParametersDto
+  ) {
+    return this.businessService.getBusinessPaginatedByTypeId(typeId, pagination);
+  }
 
-    @Get("/owner")
-    async getBusinessPaginatedByOwnerId(
-        @Query('ownerId') ownerId: string,
-        @Body() pagination: PaginationParametersDto
-    ) {
-        return this.businessService.getBusinessPaginatedByOwnerId(ownerId, pagination);
-    }
+  @Get("/premiumSubscription")
+  async getBusinessPaginatedByActivePremiumSubscriptionId(
+    @Query('activePremiumSubscriptionId') activePremiumSubscriptionId: string,
+    @Body() pagination: PaginationParametersDto
+  ) {
+    return this.businessService.getBusinessPaginatedByActivePremiumSubscriptionId(activePremiumSubscriptionId, pagination);
+  }
 
-    @Get("/type")
-    async getBusinessPaginatedByTypeId(
-        @Query('typeId') typeId: string,
-        @Body() pagination: PaginationParametersDto
-    ) {
-        return this.businessService.getBusinessPaginatedByTypeId(typeId, pagination);
-    }
-
-    @Get("/premiumSubscription")
-    async getBusinessPaginatedByActivePremiumSubscriptionId(
-        @Query('activePremiumSubscriptionId') activePremiumSubscriptionId: string,
-        @Body() pagination: PaginationParametersDto
-    ) {
-        return this.businessService.getBusinessPaginatedByActivePremiumSubscriptionId(activePremiumSubscriptionId, pagination);
-    }
-
-    @Get()
-    async getBusinessById(
-        @Query('businessId') businessId: string
-    ) {
-        return this.businessService.getBusinessById(businessId);
-    }
-
-    @Post()
-    @UseInterceptors(
-        FileFieldsInterceptor([
-            { name: 'logo', maxCount: 1 },
-            { name: 'banner', maxCount: 1 },
-        ]),
-    )
-    async createBusiness(
-        @Body() createBusinessDto: BusinessCreateDto,
-        @UploadedFiles() files: { logo?: any; banner?: any },
-    ) {
-        // Convertir la cadena JSON 'coordinates' en un objeto.
-        if (typeof createBusinessDto.coordinates === 'string') {
-            try {
-                createBusinessDto.coordinates = JSON.parse(
-                    createBusinessDto.coordinates,
-                );
-            } catch (error) {
-                throw new BadRequestException('Invalid coordinates format');
-            }
-        }
-
-        // Convertir la cadena JSON 'availability' en un array.
-        if (typeof createBusinessDto.availability === 'string') {
-            try {
-                createBusinessDto.availability = JSON.parse(
-                    createBusinessDto.availability,
-                );
-            } catch (error) {
-                throw new BadRequestException('Invalid availability format');
-            }
-        }
-
-        console.log('DTO received: ', createBusinessDto);
-        console.log('Logo received: ', files.logo);
-        console.log('Banner received: ', files.banner);
-
-        return this.businessService.createBusiness(
-            createBusinessDto,
-            files.logo ? files.logo[0] : undefined,
-            files.banner ? files.banner[0] : undefined,
+  @Post()
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'logo', maxCount: 1 },
+      { name: 'banner', maxCount: 1 },
+    ]),
+  )
+  async createBusiness(
+    @Body() createBusinessDto: BusinessCreateDto,
+    @UploadedFiles() files: { logo?: any; banner?: any },
+  ) {
+    // Convertir la cadena JSON 'coordinates' en un objeto.
+    if (typeof createBusinessDto.coordinates === 'string') {
+      try {
+        createBusinessDto.coordinates = JSON.parse(
+          createBusinessDto.coordinates,
         );
+      } catch (error) {
+        throw new BadRequestException('Invalid coordinates format');
+      }
     }
 
-    @Get()
-    async getBusiness(
-        @Query('businessId') businessId?: string,
-        @Query('ownerId') ownerId?: string,
-        @Query('typeId') typeId?: string,
-        @Query('activePremiumSubscriptionId') activePremiumSubscriptionId?: string,
-    ) {
-        if (businessId) {
-            return this.businessService.getBusinessById(businessId);
-        } else if (ownerId) {
-            return this.businessService.getBusinessByOwnerId(ownerId);
-        } else if (typeId) {
-            return this.businessService.getBusinessByTypeId(typeId);
-        } else if (activePremiumSubscriptionId) {
-            return this.businessService.getBusinessByActivePremiumSubscriptionId(
-                activePremiumSubscriptionId,
-            );
-        } else {
-            throw new BadRequestException('Invalid query parameters.');
-        }
-    }
-
-    @Patch(':id')
-    @UseInterceptors(
-        FileFieldsInterceptor([
-            { name: 'logo', maxCount: 1 },
-            { name: 'banner', maxCount: 1 },
-        ]),
-    )
-    async updateBusiness(
-        @Param('id') id: string,
-        @UploadedFiles() files: { logo?: any; banner?: any },
-        @Body() businessUpdateDto: BusinessUpdateDto,
-    ) {
-        return this.businessService.updateBusiness(
-            id,
-            businessUpdateDto,
-            files.logo,
-            files.banner,
+    // Convertir la cadena JSON 'availability' en un array.
+    if (typeof createBusinessDto.availability === 'string') {
+      try {
+        createBusinessDto.availability = JSON.parse(
+          createBusinessDto.availability,
         );
+      } catch (error) {
+        throw new BadRequestException('Invalid availability format');
+      }
     }
 
-    @Delete(':id')
-    async removeBusiness(@Param('id') id: string) {
-        return this.businessService.removeBusiness(id);
+    console.log('DTO received: ', createBusinessDto);
+    console.log('Logo received: ', files.logo);
+    console.log('Banner received: ', files.banner);
+
+    return this.businessService.createBusiness(
+      createBusinessDto,
+      files.logo ? files.logo[0] : undefined,
+      files.banner ? files.banner[0] : undefined,
+    );
+  }
+
+  @Get()
+  async getBusiness(
+    @Query('businessId') businessId?: string,
+    @Query('ownerId') ownerId?: string,
+    @Query('typeId') typeId?: string,
+    @Query('activePremiumSubscriptionId') activePremiumSubscriptionId?: string,
+  ) {
+    if (businessId) {
+      return this.businessService.getBusinessById(businessId);
+    } else if (ownerId) {
+      return this.businessService.getBusinessByOwnerId(ownerId);
+    } else if (typeId) {
+      return this.businessService.getBusinessByTypeId(typeId);
+    } else if (activePremiumSubscriptionId) {
+      return this.businessService.getBusinessByActivePremiumSubscriptionId(
+        activePremiumSubscriptionId,
+      );
+    } else {
+      throw new BadRequestException('Invalid query parameters.');
     }
+  }
+
+  @Patch(':id')
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'logo', maxCount: 1 },
+      { name: 'banner', maxCount: 1 },
+    ]),
+  )
+  async updateBusiness(
+    @Param('id') id: string,
+    @UploadedFiles() files: { logo?: any; banner?: any },
+    @Body() businessUpdateDto: BusinessUpdateDto,
+  ) {
+    return this.businessService.updateBusiness(
+      id,
+      businessUpdateDto,
+      files.logo,
+      files.banner,
+    );
+  }
+
+  @Delete(':id')
+  async removeBusiness(@Param('id') id: string) {
+    return this.businessService.removeBusiness(id);
+  }
 }
--- src/business/business.service.ts
+++ src/business/business.service.ts
@@ -1,10 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import {
-    Business,
-    BusinessStatus,
-    IAvailability,
-    IBusiness,
-    WeekDays,
+  Business,
+  BusinessStatus,
+  IAvailability,
+  IBusiness,
+  WeekDays,
 } from './entities/business.entity';
 import { BusinessCreateDto } from './entities/business-create.dto';
 import { v4 as uuidv4 } from 'uuid';
@@ -17,206 +17,192 @@ import { PaginationParametersDto } from "src/helpers/pagination-parameters.dto";
 
 @Injectable()
 export class BusinessService {
-    constructor(private readonly s3Service: S3Service) { }
+  constructor(private readonly s3Service: S3Service) { }
 
-    async getBusinessPaginatedById(businessId: string, pagination: PaginationParametersDto) {
-        let business = await Business.scan("businessId").eq(businessId).limit(pagination.limit);
+  async getBusinessPaginatedByOwnerId(ownerId: string, pagination: PaginationParametersDto) {
+    let business = await Business.scan("ownerId").eq(ownerId).limit(pagination.limit);
 
-        if (pagination.lastKey) {
-            business = business.startAt(pagination.lastKey);
-        }
-
-        const result = await business.exec();
-        return {
-            items: result,
-            lastKey: result.lastKey || null,
-        };
+    if (pagination.lastKey) {
+      business = business.startAt(pagination.lastKey);
     }
 
-    async getBusinessPaginatedByOwnerId(ownerId: string, pagination: PaginationParametersDto) {
-        let business = await Business.scan("ownerId").eq(ownerId).limit(pagination.limit);
+    const result = await business.exec();
+    return {
+      items: result,
+      lastKey: result.lastKey || null,
+    };
+  }
 
-        if (pagination.lastKey) {
-            business = business.startAt(pagination.lastKey);
-        }
+  async getBusinessPaginatedByTypeId(typeId: string, pagination: PaginationParametersDto) {
+    let business = await Business.scan("typeId").eq(typeId).limit(pagination.limit);
 
-        const result = await business.exec();
-        return {
-            items: result,
-            lastKey: result.lastKey || null,
-        };
+    if (pagination.lastKey) {
+      business = business.startAt(pagination.lastKey);
     }
 
-    async getBusinessPaginatedByTypeId(typeId: string, pagination: PaginationParametersDto) {
-        let business = await Business.scan("typeId").eq(typeId).limit(pagination.limit);
+    const result = await business.exec();
+    return {
+      items: result,
+      lastKey: result.lastKey || null,
+    };
+  }
 
-        if (pagination.lastKey) {
-            business = business.startAt(pagination.lastKey);
-        }
+  async getBusinessPaginatedByActivePremiumSubscriptionId(activePremiumSubscriptionID: string, pagination: PaginationParametersDto) {
+    let business = await Business.scan("activePremiumSubscriptionID").eq(activePremiumSubscriptionID).limit(pagination.limit);
 
-        const result = await business.exec();
-        return {
-            items: result,
-            lastKey: result.lastKey || null,
-        };
+    if (pagination.lastKey) {
+      business = business.startAt(pagination.lastKey);
     }
 
-    async getBusinessPaginatedByActivePremiumSubscriptionId(activePremiumSubscriptionID: string, pagination: PaginationParametersDto) {
-        let business = await Business.scan("activePremiumSubscriptionID").eq(activePremiumSubscriptionID).limit(pagination.limit);
+    const result = await business.exec();
+    return {
+      items: result,
+      lastKey: result.lastKey || null,
+    };
+  }
 
-        if (pagination.lastKey) {
-            business = business.startAt(pagination.lastKey);
-        }
+  async getBusinessById(id: string) {
+    const business = await Business.get(id);
+    return business;
+  }
 
-        const result = await business.exec();
-        return {
-            items: result,
-            lastKey: result.lastKey || null,
-        };
+  async getBusinessByOwnerId(ownerId: string): Promise<any> {
+    const business = await Business.scan('ownerId').eq(ownerId).exec();
+    return business;
+  }
+
+  async getBusinessByTypeId(typeId: string): Promise<any> {
+    const business = await Business.scan('typeId').eq(typeId).exec();
+    return business;
+  }
+
+  async getBusinessByActivePremiumSubscriptionId(
+    activePremiumSubscriptionID: string,
+  ): Promise<any> {
+    const business = await Business.scan('activePremiumSubscriptionID')
+      .eq(activePremiumSubscriptionID)
+      .exec();
+    return business;
+  }
+
+  async updateBusiness(
+    id: string,
+    updateData: BusinessUpdateDto,
+    logo?: any,
+    banner?: any,
+  ): Promise<IBusiness> {
+    const business = await Business.get(id);
+    console.log('el business:', business);
+    if (!business) {
+      throw new NotFoundException(`Business with ID ${id} not found`);
     }
 
-    async getBusinessById(id: string) {
-        const business = await Business.get(id);
-        return business;
+    if (logo) {
+      console.log('updating logo: ', logo);
+      const sanitizedFilename = logo[0].originalname.replace(/\s+/g, '');
+      logo[0].originalname = sanitizedFilename;
+      await this.s3Service.uploadFile(
+        logo[0],
+        `business/${business.ownerId}/logo/`,
+      );
+      business.logoURL = `https://${process.env.S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com/business/${business.ownerId}/logo/${sanitizedFilename}`;
     }
 
-    async getBusinessByOwnerId(ownerId: string): Promise<any> {
-        const business = await Business.scan('ownerId').eq(ownerId).exec();
-        return business;
+    if (banner) {
+      console.log('updating banner', banner);
+      const sanitizedBannerFilename = banner[0].originalname.replace(
+        /\s+/g,
+        '',
+      );
+      banner[0].originalname = sanitizedBannerFilename;
+      await this.s3Service.uploadFile(
+        banner[0],
+        `business/${business.ownerId}/banner/`,
+      );
+      business.multimediaURL = [
+        `https://${process.env.S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com/business/${business.ownerId}/banner/${sanitizedBannerFilename}`,
+      ];
     }
 
-    async getBusinessByTypeId(typeId: string): Promise<any> {
-        const business = await Business.scan('typeId').eq(typeId).exec();
-        return business;
+    for (const key in updateData) {
+      if (Object.prototype.hasOwnProperty.call(updateData, key)) {
+        business[key] = updateData[key];
+      }
     }
 
-    async getBusinessByActivePremiumSubscriptionId(
-        activePremiumSubscriptionID: string,
-    ): Promise<any> {
-        const business = await Business.scan('activePremiumSubscriptionID')
-            .eq(activePremiumSubscriptionID)
-            .exec();
-        return business;
+    await business.save();
+    return business;
+  }
+
+  async createBusiness(
+    businessCreateDto: BusinessCreateDto,
+    logo: any,
+    banner: any,
+  ): Promise<any> {
+    const owner = await User.get(businessCreateDto.ownerId);
+    if (owner == undefined)
+      throw new NotFoundException('Owner does not exists');
+
+    const businessType = BusinessType.get(businessCreateDto.typeId);
+    if (businessType == undefined)
+      throw new NotFoundException('Business Type does not exists');
+
+    let logoUrl =
+      'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000'; // URL por defecto
+    let bannerUrl =
+      'https://i.pinimg.com/564x/08/8c/09/088c099c81191b734e0db14f0e253142.jpg'; // Puedes poner una URL de imagen de banner por defecto aquí
+
+    if (logo) {
+      const sanitizedFilename = logo.originalname.replace(/\s+/g, '');
+      logo.originalname = sanitizedFilename;
+      await this.s3Service.uploadFile(
+        logo,
+        `business/${businessCreateDto.ownerId}/logo/`,
+      );
+      logoUrl = `https://${process.env.S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com/business/${businessCreateDto.ownerId}/logo/${sanitizedFilename}`;
     }
 
-    async updateBusiness(
-        id: string,
-        updateData: BusinessUpdateDto,
-        logo?: any,
-        banner?: any,
-    ): Promise<IBusiness> {
-        const business = await Business.get(id);
-        console.log('el business:', business);
-        if (!business) {
-            throw new NotFoundException(`Business with ID ${id} not found`);
-        }
-
-        if (logo) {
-            console.log('updating logo: ', logo);
-            const sanitizedFilename = logo[0].originalname.replace(/\s+/g, '');
-            logo[0].originalname = sanitizedFilename;
-            await this.s3Service.uploadFile(
-                logo[0],
-                `business/${business.ownerId}/logo/`,
-            );
-            business.logoURL = `https://${process.env.S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com/business/${business.ownerId}/logo/${sanitizedFilename}`;
-        }
-
-        if (banner) {
-            console.log('updating banner', banner);
-            const sanitizedBannerFilename = banner[0].originalname.replace(
-                /\s+/g,
-                '',
-            );
-            banner[0].originalname = sanitizedBannerFilename;
-            await this.s3Service.uploadFile(
-                banner[0],
-                `business/${business.ownerId}/banner/`,
-            );
-            business.multimediaURL = [
-                `https://${process.env.S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com/business/${business.ownerId}/banner/${sanitizedBannerFilename}`,
-            ];
-        }
-
-        for (const key in updateData) {
-            if (Object.prototype.hasOwnProperty.call(updateData, key)) {
-                business[key] = updateData[key];
-            }
-        }
-
-        await business.save();
-        return business;
+    if (banner) {
+      const sanitizedBannerFilename = banner.originalname.replace(/\s+/g, '');
+      banner.originalname = sanitizedBannerFilename;
+      await this.s3Service.uploadFile(
+        banner,
+        `business/${businessCreateDto.ownerId}/banner/`,
+      );
+      bannerUrl = `https://${process.env.S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com/business/${businessCreateDto.ownerId}/banner/${sanitizedBannerFilename}`;
     }
 
-    async createBusiness(
-        businessCreateDto: BusinessCreateDto,
-        logo: any,
-        banner: any,
-    ): Promise<any> {
-        const owner = await User.get(businessCreateDto.ownerId);
-        if (owner == undefined)
-            throw new NotFoundException('Owner does not exists');
+    const business = new Business({
+      id: uuidv4(),
+      ownerId: businessCreateDto.ownerId,
+      typeId: businessCreateDto.typeId,
+      name: businessCreateDto.name,
+      country: businessCreateDto.country,
+      department: businessCreateDto.department,
+      address: businessCreateDto.address,
+      coordinates: businessCreateDto.coordinates,
+      logoURL: logoUrl,
+      multimediaURL: [bannerUrl], // Si tienes varias URL de multimedia, combínalas aquí.
+      description: businessCreateDto.description,
+      assistantsID: [],
+      pendingInvitationsID: [],
+      status: BusinessStatus.Pending,
+      totalRatingSum: 0,
+      totalRatingsCount: 0,
+      averageRating: 0,
+      availability: businessCreateDto.availability,
+    });
 
-        const businessType = BusinessType.get(businessCreateDto.typeId);
-        if (businessType == undefined)
-            throw new NotFoundException('Business Type does not exists');
+    return await business.save();
+  }
 
-        let logoUrl =
-            'https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000'; // URL por defecto
-        let bannerUrl =
-            'https://i.pinimg.com/564x/08/8c/09/088c099c81191b734e0db14f0e253142.jpg'; // Puedes poner una URL de imagen de banner por defecto aquí
+  async removeBusiness(id: string) {
+    const business = await Business.get(id);
 
-        if (logo) {
-            const sanitizedFilename = logo.originalname.replace(/\s+/g, '');
-            logo.originalname = sanitizedFilename;
-            await this.s3Service.uploadFile(
-                logo,
-                `business/${businessCreateDto.ownerId}/logo/`,
-            );
-            logoUrl = `https://${process.env.S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com/business/${businessCreateDto.ownerId}/logo/${sanitizedFilename}`;
-        }
-
-        if (banner) {
-            const sanitizedBannerFilename = banner.originalname.replace(/\s+/g, '');
-            banner.originalname = sanitizedBannerFilename;
-            await this.s3Service.uploadFile(
-                banner,
-                `business/${businessCreateDto.ownerId}/banner/`,
-            );
-            bannerUrl = `https://${process.env.S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com/business/${businessCreateDto.ownerId}/banner/${sanitizedBannerFilename}`;
-        }
-
-        const business = new Business({
-            id: uuidv4(),
-            ownerId: businessCreateDto.ownerId,
-            typeId: businessCreateDto.typeId,
-            name: businessCreateDto.name,
-            country: businessCreateDto.country,
-            department: businessCreateDto.department,
-            address: businessCreateDto.address,
-            coordinates: businessCreateDto.coordinates,
-            logoURL: logoUrl,
-            multimediaURL: [bannerUrl], // Si tienes varias URL de multimedia, combínalas aquí.
-            description: businessCreateDto.description,
-            assistantsID: [],
-            pendingInvitationsID: [],
-            status: BusinessStatus.Pending,
-            totalRatingSum: 0,
-            totalRatingsCount: 0,
-            averageRating: 0,
-            availability: businessCreateDto.availability,
-        });
-
-        return await business.save();
+    if (!business) {
+      throw new Error('business not found');
     }
 
-    async removeBusiness(id: string) {
-        const business = await Business.get(id);
-
-        if (!business) {
-            throw new Error('business not found');
-        }
-
-        return business.delete();
-    }
+    return business.delete();
+  }
 }
