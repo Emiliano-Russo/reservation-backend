ebde74f0ef9b0819e02c16c3193ea22ba9ca78f6
Emiliano Russo <russo.emiliano97@gmail.com>
2023-09-21 20:11:43 -0300

    fix/all module bugs

--- src/business/business.module.ts
+++ src/business/business.module.ts
@@ -3,9 +3,11 @@ import { BusinessController } from './business.controller';
 import { BusinessService } from './business.service';
 import { UserModule } from 'src/user/user.module';
 import { SharedModule } from 'src/shared/shared.module';
+import { Business } from './entities/business.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, TypeOrmModule.forFeature([Business])],
   controllers: [BusinessController],
   providers: [BusinessService],
   exports: [BusinessService],
--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -3,9 +3,15 @@ import { ReservationController } from './reservation.controller';
 import { ReservationService } from './reservation.service';
 import { BusinessModule } from 'src/business/business.module';
 import { UserModule } from 'src/user/user.module';
+import { Reservation } from './entities/reservation.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [BusinessModule, UserModule],
+  imports: [
+    BusinessModule,
+    UserModule,
+    TypeOrmModule.forFeature([Reservation]),
+  ],
   controllers: [ReservationController],
   providers: [ReservationService],
   exports: [ReservationService],
