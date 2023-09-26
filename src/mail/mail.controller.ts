467e21a6aa230bbd262fe20859d51a888ef385c1
Emiliano Russo <russo.emiliano97@gmail.com>
2023-09-26 11:48:15 -0300

    refactor/mail-module

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -5,15 +5,6 @@ import { MailService } from './mail.service';
 export class MailController {
   constructor(private readonly mailService: MailService) {}
 
-  @Post('send')
-  async sendEmail(
-    @Body('to') to: string,
-    @Body('subject') subject: string,
-    @Body('content') content: string,
-  ) {
-    return this.mailService.sendEmail(to, subject, content);
-  }
-
   @Post('send-confirmation')
   async sendConfirmationEmail(@Body('email') email: string) {
     return this.mailService.sendConfirmationEmail(email);
